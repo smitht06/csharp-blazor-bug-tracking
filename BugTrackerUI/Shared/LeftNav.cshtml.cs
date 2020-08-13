128b4c2df65bc472db468db80c67f5f8bc06187a
Anthony Smith <51099384+smitht06@users.noreply.github.com>
2020-08-13 11:56:18 -0400

    lol

---
+++ BugTrackerUI/Shared/LeftNav.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BugTrackerUI.Shared
+{
+    public class LeftNav : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
